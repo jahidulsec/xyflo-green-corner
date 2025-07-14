7898dd8fdc785ab91f9395aa6e46e6cac63470c0
Jahidul Islam <jahidul.app@gmail.com>
2025-07-14 12:20:10 +0600

    feat: adding inital project setup

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+export default function page() {
+  return (
+    <div className="container mx-auto">
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati
+      consequuntur reprehenderit temporibus illum inventore optio, fuga
+      officiis. Commodi quidem eum consequatur aspernatur. Autem, possimus
+      fugiat consequatur sequi odio cumque neque?
+      <Button className="">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro amet quisquam nihil iste, quam quia neque totam dolorem sequi repellat obcaecati, perferendis consequuntur veniam vitae, adipisci facere dolorum excepturi reprehenderit!</Button>
+    </div>
+  );
+}
