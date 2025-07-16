2db270de13399ab1b0f442a11945b4588310d2f3
Jahidul Islam <jahidul.app@gmail.com>
2025-07-16 16:14:41 +0600

    feat: adding logout action for admin

--- src/app/(doctor)/login/admin/page.tsx
+++ src/app/(doctor)/login/admin/page.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function AdminLoginPage() {
   return (
     <div>
-      <main className=" w-full flex flex-col justify-center items-center px-6 relative min-h-[calc(100vh-30rem)]">
+      <main className=" w-full flex flex-col justify-center items-center px-6 relative">
         <LoginForm />
       </main>
     </div>
--- src/components/dashboard/app-sidebar.tsx
+++ src/components/dashboard/app-sidebar.tsx
@@ -1,18 +1,22 @@
 "use client";
 
 import * as React from "react";
-import { Sprout, Users2 } from "lucide-react";
+import { LogOut, Sprout, Users2 } from "lucide-react";
 
 import { NavMain } from "@/components/dashboard/nav-main";
-import { NavUser } from "@/components/dashboard/nav-user";
 import { TeamSwitcher } from "@/components/dashboard/team-switcher";
 import {
   Sidebar,
   SidebarContent,
   SidebarFooter,
   SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
   SidebarRail,
 } from "@/components/ui/sidebar";
+import { useRouter } from "@bprogress/next";
+import { logout } from "@/feature/admin/actions/admin";
+import { toast } from "sonner";
 
 const data = {
   user: {
@@ -38,6 +42,9 @@ const data = {
 };
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  const [pending, startTransition] = React.useTransition();
+  const router = useRouter();
+
   return (
     <Sidebar collapsible="icon" {...props}>
       <SidebarHeader>
@@ -47,7 +54,29 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
         <NavMain items={data.navMain} />
       </SidebarContent>
       <SidebarFooter>
-        <NavUser user={data.user} />
+        <SidebarMenu>
+          <SidebarMenuButton
+            disabled={pending}
+            onClick={() => {
+              startTransition(async () => {
+                const response = logout();
+                toast.promise(response, {
+                  loading: "Loading...",
+                  success: (data) => {
+                    if (!data.success) throw data.error;
+                    router.replace("/login/admin");
+                    return data.success;
+                  },
+                  error: (data) => {
+                    return data.error;
+                  },
+                });
+              });
+            }}
+          >
+            <LogOut /> Logout
+          </SidebarMenuButton>
+        </SidebarMenu>
       </SidebarFooter>
       <SidebarRail />
     </Sidebar>
