dcdf2f5e4ebe6781db55fccad14b9232b8687b05
Jahidul Islam <jahidul.app@gmail.com>
2025-07-17 15:29:39 +0600

    chore: update success message on registration

--- src/app/(doctor)/success/page.tsx
+++ src/app/(doctor)/success/page.tsx
@@ -5,11 +5,10 @@ export default function SuccessPage() {
   return (
     <div className="flex justify-center items-center flex-col gap-10 text-center text-sm border rounded-md bg-primary/10 backdrop-blur-sm p-10">
       <CircleCheck size={100} className="text-primary/50 fill-primary/30" />
-      <p>Thank you for your submission.</p>
-      <p className="">
-        Best regards, <br />{" "}
-        <strong className="text-xl text-primary">Xyflo</strong>
+      <p>
+        Your Submission is Completed. <br /> Thank You for being a <br />
       </p>
+      <strong className="text-primary text-xl -mt-8">Xyflo Green Hero</strong>
     </div>
   );
 }
